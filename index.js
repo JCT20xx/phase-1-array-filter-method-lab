636fe28ddcc51bf7b2081a7746bd22ff850fb22a
Juan Toro <jct20xx@gmail.com>
2022-03-18 13:28:49 -0400

    commit tree b1f39f4ee69f6d661c045d28a6deb140ce28f158

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name){
+    return array.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(driver => driver.startsWith(name))
+}
+
+function matchName(array,name){
+    return array.filter(driver => driver.name === name)
+}
